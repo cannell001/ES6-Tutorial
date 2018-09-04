48b45107584040f9d3bf3d0c13f7b76e3492d04a
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-04 15:10:04 -0400

    Arrow 1

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 window.onload = function(){
 
-    var temp = `hello, my name is Ryu`;
-    var temp2 = `hello,
-                 my name is Ryu`;
-
-    console.log(temp);
-    console.log(temp2);
-    console.log("");
-
-    function logNinja(name, age){
-        console.log("my name is " + name + " and my age is " + age);
-        console.log(`my name is ${name} and my age is ${age}`);
-        console.log(`my name is ${name} and my age is ${10 + 14}`);
+    var ninjaGreeting = function(){
+        console.log("hiiiiiiiiya");
     }
 
-    logNinja("John", 25);
+    ninjaGreeting();
 }
