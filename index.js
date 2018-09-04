65604dc715ec9102283a6f4488232ebdfced22c0
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-04 11:56:42 -0400

    Change scope of const

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ window.onload = function(){
     const pi = 3.142;
 
     function calcArea(radius){
-        console.log("The area is: " + pi * radius * radius);
-        console.log("Using Template Literal Syntax -> ")
+        const pi = 10
         console.log(`The area is ${pi * radius * radius}`);
     }
 
