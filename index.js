8513655c161dde93784d2edfc03ee32d991d5b98
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-04 13:58:08 -0400

    Spread Operator 3

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 window.onload = function(){
 
     var nums1 = [1,2,3];
-    var nums2 = [nums1,3,4,5];
+    var nums2 = [...nums1,3,4,5];
 
     console.log(nums2);
+    console.log("");
+
+    var nums = [3,5,7];
+
+    function addNums(a,b,c){
+        console.log(a+b+c)
+    }
+
+    addNums(nums);
+    addNums(...nums);
+
+    console.log("");
+
+    addNums = function(a,b,c){
+        console.log(a+b+c)
+    }
+
+    addNums(...nums);
+
+    console.log("");
+
+    addNums = (a,b,c) => console.log(a+b+c);
+
+    addNums(...nums);
 
 }
