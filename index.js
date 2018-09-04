d377ff045736b6516dc081cc579ff1584172ca3e
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-04 12:39:17 -0400

    Single line Syntax

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ window.onload = function(){
     var items = document.getElementsByTagName("li");
 
     for(let x = 0; x < items.length; x++){
-        items[x].onclick = () => {
-            console.log(x);
-        }
+        items[x].onclick = () => console.log(x);
     }
 
-    //console.log(x);
-
+    console.log(`Arrow Function Syntax: () => console.log(x)`);    
 }
