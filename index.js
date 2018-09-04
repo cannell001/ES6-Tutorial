57a15917dfa19afe40358ef237b4fdcb0062e901
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-04 12:27:57 -0400

    using let - x is undefined outside block

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ window.onload = function(){
 
     var items = document.getElementsByTagName("li");
 
-    for(var x = 0; x < items.length; x++){
+    for(let x = 0; x < items.length; x++){
         items[x].onclick = function(){
             console.log(x);
         }
     }
 
-    console.log(x);
+    //console.log(x);
 
 }
