1e77308f8015ca1ce5cdc59c4694adedc1756535
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-04 12:31:35 -0400

    Arrow Function - 1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.onload = function(){
     var items = document.getElementsByTagName("li");
 
     for(let x = 0; x < items.length; x++){
-        items[x].onclick = function(){
+        items[x].onclick = () => {
             console.log(x);
         }
     }
