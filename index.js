fc4b36626885f97c875d1b5fa5c9d3908d5627a2
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-04 15:26:45 -0400

    _this = this;

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ window.onload = function(){
     var ninja = {
         name: "Ryu",
         chop(x){
+            var _this = this;
             window.setInterval(function(){
                 if(x > 0){
-                    console.log(this.name + " chopped the enemy");
+                    console.log(_this.name + " chopped the enemy");
                     x--;
                 }
             }, 1000);
