7ab2d8e9936814fb44d3be4b77334f24ac9eb716
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-04 13:47:43 -0400

    Spread Operator 2

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 window.onload = function(){
 
-    var meats = ["ham", "salami", "bacon"];
-
-    console.log(meats);
-    console.log(...meats);
-    console.log("");
-
     var nums1 = [1,2,3];
-    var nums2 = [3,4,5];
+    var nums2 = [nums1,3,4,5];
 
     console.log(nums2);
 
