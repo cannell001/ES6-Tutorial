fa3d8a92f4c17d3b4a514bed08ef8afbd567a495
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-04 12:13:14 -0400

    Lexical (block) scope

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ window.onload = function(){
     var x = 10;
 
     if(x > 5){
-        var x = 5;
+        let x = 5;
+        console.log(`inside block: ${x}`);
     }
 
-    console.log(x);
+    console.log(`outside block: ${x}`);
 }
