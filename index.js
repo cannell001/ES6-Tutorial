6e00a8f5df257e7b30c3b7381d39eb5b7b2cc5ab
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-06 13:43:53 -0400

    for Loop

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 window.onload = function(){
 
-    var ninja = {
-        name: "Ryu",
-        chop(x){
-            window.setInterval(() => {
-                if(x > 0){
-                    console.log(this.name + " chopped the enemy");
-                    x--;
-                }
-            }, 1000);
-        }
-    };
+    const companies= [
+      {name: "Company One", category: "Finance", start: 1981, end: 2004},
+      {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+      {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+      {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+      {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+      {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+      {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+      {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+      {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+    ];
 
-    ninja.chop(5);
+    const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+     for(let i = 0; i < companies.length; i++) {
+       console.log(companies[i]);
+     }
+     
+     for(let i = 0; i < ages.length; i++) {
+       console.log(ages[i]);
+     }
 }
